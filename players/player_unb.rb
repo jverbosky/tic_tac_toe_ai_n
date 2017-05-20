bdb258919cf712fd2fbfb45d23eb7c1dfda0586f
jverbosk <jverbosk@gmail.com>
2017-05-19 21:38:11 -0400

    cleaned up puts statements used for tracing during testing and troubleshooting

--- players/player_unb.rb
+++ players/player_unb.rb
@@ -44,7 +44,6 @@ class PlayerUnbeatable
 
   # Method to return position to win, call block_check() if no wins
   def win_check(player, opponent)
-    # puts "win_check()"
     position = []  # placeholder for position that will give 3-in-a-row
     @wins.each do |win|  # check each win pattern
       difference = win - player  # difference between current win array and player position array
@@ -56,7 +55,6 @@ class PlayerUnbeatable
 
   # Method to return position to block, call poison_line() if no blocks
   def block_check(player, opponent)
-    # puts "block_check()"
     position = []  # placeholder for position that will block the opponent
     @wins.each do |win|  # check each win pattern
       difference = win - opponent  # difference between current win array and opponent position array
@@ -68,34 +66,23 @@ class PlayerUnbeatable
 
   # Method to create fork, force block if 2+ op forks, block fork if 1 op fork, or call get_cen()
   def fork_check(player, opponent)
-    # puts "fork_check()"
     block_fork = find_fork(opponent, player)
-    # puts "block_fork: #{block_fork}"
     get_fork = find_fork(player, opponent)
-    # puts "get_fork: #{get_fork}"
-    # p get_fork
-
     if block_fork.size > 0 && @size > 3
       move = block_fork.sample
-
     elsif get_fork.size > 0  # if possible to create fork, do it
-      # puts "get_fork"
       move = get_fork.sample
     elsif block_fork.size > 1 && @size == 3  # if opponent can create multiple forks, force block
-      # puts "get_adj"
       move = get_adj(player, opponent)
     elsif block_fork.size == 1 && @size == 3  # otherwise if opponent can create fork, block it
-      # puts "block_fork"
       move = block_fork[0]
     else @center != nil && [@center] & player + opponent == []
-      # puts "get_cen"
       move = get_cen(player, opponent)
     end
   end
 
   # Method to return the center position, or call get_op_cor()
   def get_cen(player, opponent)
-    # puts "get_cen()"
     taken = player + opponent  # all occupied board positions
     # if center is open & board has a true center (3x3, 5x5, etc.)
     if (taken & [@center]).size == 0 && @size % 2 == 1
@@ -107,7 +94,6 @@ class PlayerUnbeatable
 
   # Method to return the corner opposite the opponent's corner or call get_avail_cor()
   def get_op_cor(player, opponent)
-    # puts "get_op_cor"
     p_corner = (player & @corners)  # determine the player's corners
     o_corner = (opponent & @corners)  # determine the opponent's corners
     if (@opcor_1 & p_corner).size == 0 && (@opcor_1 & o_corner).size == 1
@@ -121,7 +107,6 @@ class PlayerUnbeatable
 
   # Method to return random open corner or call get_avail_edg()
   def get_avail_cor(player, opponent)
-    # puts "get_avail_cor"
     taken = player + opponent  # all occupied board positions
     avail_cor = @corners - (@corners & taken)  # determine which corners are taken
     if avail_cor.size > 0  # if there are any open corners
@@ -135,21 +120,16 @@ class PlayerUnbeatable
 
   # Method to return a random open edge
   def get_avail_edg(player, opponent)
-    # puts "get_avail_edg"
     taken = player + opponent  # all occupied board positions
     avail_edg = @edges - (@edges & taken)  # determine which edges are taken
     position = avail_edg.sample  # take one of them
   end
 
   def poison_line(player, opponent)
-    # puts "poison_line"
     open_positions = @moves - (player + opponent)
-    # puts "open_positions: #{open_positions}"
     target_lines = get_potential_wins(player, opponent).flatten
-    # p "target_lines: #{target_lines}"
     if target_lines != []
       poison_positions = open_positions & (target_lines - player)
-      # puts "poison_positions: #{poison_positions}"
       position = poison_positions.sample
     else
       position = open_positions.sample
@@ -166,7 +146,6 @@ class PlayerUnbeatable
       forking_moves.push(position) if count > 1
     end
     forking_moves = (forking_moves - (forking_moves & forker))
-    # puts "forking_moves: #{forking_moves}"
     forking_moves.empty? ? [] : forking_moves
   end
 
@@ -180,14 +159,11 @@ class PlayerUnbeatable
         position_counts[position] += 1
       end
     end
-    # puts "position_counts: #{position_counts}"
     return position_counts
   end
 
   # Method to return array of potential wins for forking player
   def get_potential_wins(forker, forkee)
-    # puts "get_potential_wins forker positions: #{forker}"
-    # puts "get_potential_wins forkee positions: #{forkee}"
     potential_wins = []
     @wins.each do |win|
       if @size == 3
@@ -196,7 +172,6 @@ class PlayerUnbeatable
         potential_wins.push(win) if (forker & win).size > 1 && (forkee & win).size == 0
       end
     end
-    # puts "potential_wins: #{potential_wins}"
     return potential_wins
   end
 
@@ -204,23 +179,15 @@ class PlayerUnbeatable
   def get_adj(player, opponent)
     potential_wins = get_potential_wins(player, opponent)  # get potential wins for player
     o_forks = find_fork(opponent, player)  # get potential forks for opponent
-    # p "o_forks: #{o_forks}"
-    # p "opcor_1: #{@opcor_1}"
-    # p "opcor_2: #{@opcor_2}"
     open_p = []  # array to collect all open positions that could create a player win
     potential_wins.each do |p_win|  # check each win for player and opponent positions
       open_p.push(p_win - player) if (p_win & player).size == 1 && (p_win & opponent).size == 0
     end
     if o_forks == @opcor_1 || o_forks == @opcor_2  # return position to force block without opponent fork
-      # puts "o_forks"
       move = (open_p.flatten - o_forks).sample
     elsif (open_p.flatten & o_forks).size > 0
-      # puts "open_p 1"
-      # puts "open_p: #{open_p}"
-      # puts "o_forks: #{o_forks}"
       move = (open_p.flatten & o_forks).sample
     else
-      # puts "open_p 2"
       move = open_p.flatten.sample
     end
   end
